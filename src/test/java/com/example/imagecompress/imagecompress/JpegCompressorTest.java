8875d90b17d163f55599bcbafdc7b66c0b5d0ea9
Luis Trigueiros <luistrigueiros@gmail.com>
2023-05-11 22:54:13 +0100

    Added JpegCompressorTest

--- src/test/java/com/example/imagecompress/imagecompress/JpegCompressorTest.java
+++ src/test/java/com/example/imagecompress/imagecompress/JpegCompressorTest.java
@@ -1,5 +1,20 @@
 package com.example.imagecompress.imagecompress;
 
-public class JpegCompressorTest {
-    //private JpegCompressor jpegCompressor = new JpegCompressor()
+import com.example.imagecompress.imagecompress.support.TemporaryFileStorageFactory;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.time.Duration;
+
+class JpegCompressorTest {
+    @Test
+    void simpleTest() throws Exception {
+        File baseDirectory = TemporaryFileStorageFactory.getBaseDirectory();
+        Duration waitDuration = Duration.ofMillis(800);
+        TemporaryFileStorage storage = TemporaryFileStorageFactory.createTemporaryFileStorage(baseDirectory, waitDuration);
+        JpegCompressor jpegCompressor = new JpegCompressor(storage);
+        File file = Paths.get("sample/input.gif").toFile();
+        jpegCompressor.compress(file);
+    }
 }
