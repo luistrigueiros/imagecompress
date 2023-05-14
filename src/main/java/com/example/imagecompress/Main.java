19966c207826d23c4f2a0bdb13d839d1f42d76a9
Luis Trigueiros <luistrigueiros@gmail.com>
2023-05-14 12:21:12 +0100

    Incremental save

--- src/main/java/com/example/imagecompress/Main.java
+++ src/main/java/com/example/imagecompress/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImageCompressApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImageCompressApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
