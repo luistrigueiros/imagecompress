66ce4842c9b4e80d74d303135ae30641ced6eb66
Luis Trigueiros <luistrigueiros@gmail.com>
2023-05-15 10:42:53 +0100

    Incremental save

--- src/jmh/java/com/example/imagecompress/SimpleBenchmark.java
+++ src/jmh/java/com/example/imagecompress/SimpleBenchmark.java
@@ -3,6 +3,7 @@ package com.example.imagecompress;
 import com.example.imagecompress.support.Util;
 import org.apache.commons.io.FilenameUtils;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.File;
@@ -11,7 +12,6 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
 
 @State(Scope.Benchmark)
 public class SimpleBenchmark {
@@ -27,23 +27,19 @@ public class SimpleBenchmark {
     }
 
     @Benchmark
-    public void simpleTest() throws IOException {
+    public void simpleTest(Blackhole blackhole) throws IOException {
         Path sample = Paths.get("sample");
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(sample)) {
             for (Path path : stream) {
                 String extension = FilenameUtils.getExtension(path.toString());
                 File file = path.toFile();
-                Optional<File> compress = imageCompressService.compress(file, extension);
-                logResult(file, compress);
+                File output = imageCompressService.compress(file, extension).get();
+                float compressedPercentage = Util.getCompressedPercentage(file, output);
+                String msg = String.format("Compressed file=%s percentage=%f", file.getAbsolutePath(), compressedPercentage);
+                System.out.println(msg);
+                blackhole.consume(output);
             }
         }
     }
 
-    private void logResult(File input, Optional<File> optionalFile) {
-        optionalFile.ifPresent( output -> {
-            float compressedPercentage = Util.getCompressedPercentage(input, output);
-            String msg = String.format("Compressed file=%s percentage=%f", input.getAbsolutePath(), compressedPercentage);
-            System.out.println(msg);
-        });
-    }
 }
